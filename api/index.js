d1f65fd38670267b3fd0bd389f094cbac139dc0a
Amadou Jamanka <ajamanka72@gmail.com>
2025-02-25 02:26:53 +0000

    Update project structure and MongoDB connection setup

--- api/index.js
+++ api/index.js
@@ -2,9 +2,17 @@ const express = require("express");
 const cors = require("cors")
 const dotenv = require("dotenv")
 const app = express();
-const authRoute = require("./routes/authRoute")
+const authRoute = require("../routes/authRoute")
 
-const MongoDB = "Connection String"
+const mongoose = require("mongoose");
+
+const MongoDB = "mongodb+srv://ajamanka:Amadou567@ajshoestore.jumg8.mongodb.net"
+
+mongoose
+  .connect(MongoDB)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
 
 app.use(cors({
     origin: ["http://localhost:3001", "https://ajshoestore.vercel.app"],
@@ -13,13 +21,8 @@ app.use(cors({
 }))
 
 app.use("/api", authRoute)
-const PORT = 5000;
 app.use(express.json())
 
 app.get("/", (req, res) => {
   res.send("Hello, Express.js Backend!");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
 });
